957c54e914f518a160f8c592f5e990e278a63a37
Aaron Lin <aaronl@berkeley.edu>
2017-08-01 04:10:15 -0700

    groundwater autocomplete
    ty google api for location finding

---
+++ config.js
@@ -0,0 +1,3 @@
+{
+	LocalRiskTool: 'AIzaSyAVtnS6X8Fk_BD0Nr_KGGDcfYoYT3b6718';
+}
--- localrisk.js
+++ localrisk.js
@@ -2,44 +2,61 @@ var LRTAPI = 'http://tapscore-dev2.us-west-1.elasticbeanstalk.com/api/ground?lat
 var lat = 18.220833;
 var lon = -66.590149;
 var currentLocation;
-function Location(lat, lon, data){
-	this.lat = lat;
-	this.lon = lon;
-	this.data = data;
-	this.untested = data['untested'];
-	this.tested = data['tested'];
-	this.non_Detect = data['non_Detect'];
-	this.high_risk = data['high_risk'];
-	this.total_high_risk = data['total_high_risk'];
+var autocomplete;
 
-	console.log(this.tested);
+var input = document.getElementById('ground-autocomplete');
+autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'] });
+google.maps.event.addListener(autocomplete, 'place_changed', updateLatLon);
+
+function Location(lat, lon, data) {
+    this.lat = lat;
+    this.lon = lon;
+    this.untested = data.untested;
+    this.tested = data.tested;
+    this.non_Detect = data.non_detect;
+    this.high_risk = data.high_risk;
+    this.total_high_risk = data.total_high_risk;
+    this.riskURL = createRiskURL(lat, lon);
 
 }
-function generateRisk(lat, lon) {
+
+function createRiskURL(lat, lon) {
     return LRTAPI + lat + '&lon=' + lon;
 }
 
+function updateLatLon() {
+    var place = autocomplete.getPlace();
+    lat = place.geometry.location.lat();
+    lon = place.geometry.location.lng();
+}
 
 $('#submit').on('click', function() {
     $.ajax({
-        url: generateRisk(lat, lon), //This URL is for Json file
+        url: createRiskURL(lat, lon), //This URL is for Json file
         type: "GET",
         dataType: "json",
         success: function(data) {
             currentLocation = new Location(lat, lon, data);
-            $('#ground').append('<p>JSON from link: ' + generateRisk(lat, lon) + '</p>');
-            $('#ground').append('<p>Lat: ' + lat + '<p>');
-            $('#ground').append('<p>Lon: ' + lon + '</p>');
-            
-            // $('#ground').append(JSON.stringify(data));
+            $('#results-header').text('Showing results from lat: ' + lat + ' lon: ' + lon);
+            console.log(currentLocation);
+            $('#results').append('<p>Untested: ' + currentLocation.untested + '</p>');
+            $('#results').append('<p>Tested: ' + currentLocation.tested + '<p>');
+            $('#results').append('<p>Undetected Contaminants: ' + currentLocation.non_Detect + '</p>');
+            $('#results').append('<ul>High-risk Contaminants Found: ' + currentLocation.total_high_risk + '</ul>');
+            currentLocation.high_risk.forEach(function(contam) {
+                $('#results>ul').append('<li>' + contam + '</li>');
+            })
+
+            // Uncomment to show JSON 
+            // $('#results').append(JSON.stringify(data));
         },
         error: function() {
             //Do alert is error
-            window.alert('ERRRORRRORRRRR ajax call from #submit button failed');
+            window.alert('ERROR: ajax call from #submit button failed');
         }
     });
 });
 
 $(function() {
-    $( "#local-risk-container" ).tabs();
-  } );
+    $("#local-risk-container").tabs();
+});
